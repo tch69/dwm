a5cae727e4ff59e62018e5ab239043ee5b2a3e38
tch69 <ifa26417@outlook.com.vn>
2021-06-04 09:41:03 +0700

    editing config.h, a little bit

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const unsigned int gappx     = 5;
 static const unsigned int border    = 8;	/* border pixel of the bar */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Roboto Mono:size=10", "fontawesome:size=10" };
+static const char *fonts[]          = { "Roboto Mono:size=8.8", "fontawesome:size=10" };
 static const char bg_norm[]         = "#080808";
 static const char fg_norm[]         = "#efefef";
 static const char bo_norm[]         = "#080808";
@@ -27,8 +27,9 @@ static const char *const autostart[] = {
 	"xmodmap", "/usr/home/fa2_l/.Xmodmap", NULL,
 	"xcompmgr", NULL,
 	"setbg", NULL,
-	"pulseaudio", "--daemon", NULL,
 	"/usr/home/fa2_l/.local/bin/dwmbar", NULL,
+	"start-pulseaudio-x11", NULL,
+	"mpd", NULL,
 	NULL
 };
 
@@ -67,72 +68,73 @@ static const Layout layouts[] = {
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+#define SLCMD(cmd) { .v = (const char*[]){ cmd, NULL } }
 /* terminal */
 #define TERM "st"
 /* screenshot using scrot */
-#define SCROT "scrot -q 100"
+#define SCROT "/usr/bin/scrot -q 100"
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,		XK_m,      spawn,          SHCMD(TERM " -e ncmpcpp") },
-	{ MODKEY|ShiftMask, 		XK_s,      spawn,          SHCMD(SCROT " -m '/usr/home/fa2_l/Screenshots/screenshot-all-%d-%m-%y-%H-%M-%S.png'") },
-	{ MODKEY|ShiftMask, 		XK_f,      spawn,          SHCMD(SCROT " -ub '/usr/home/fa2_l/Screenshots/screenshot-fixed-%d-%m-%y-%H-%M-%S.png'") },
-	{ MODKEY, 			XK_z,      spawn,          SHCMD("ibus engine Unikey") },
-	{ MODKEY|ShiftMask, 		XK_z,      spawn,          SHCMD("ibus engine xkb:us::eng") },
-	{ MODKEY|ShiftMask, 		XK_Up,     spawn,          SHCMD("amixer set Master 1%+") },
-	{ MODKEY|ShiftMask, 		XK_Down,   spawn,          SHCMD("amixer set Master 1%-") },
-	{ MODKEY|ShiftMask, 		XK_l,      spawn,          SHCMD(TERM " -e doas halt -p") },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_space,  zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_x,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ShiftMask,             XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_t,      togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	/* modifier						key			function		argument */
+	{ MODKEY|ShiftMask,		XK_Return, 	spawn, 			{.v = dmenucmd } },
+	{ MODKEY, 			XK_Return, 	spawn, 			{.v = termcmd } },
+	{ MODKEY|ShiftMask,		XK_m, 		spawn, 			SHCMD(TERM " -e ncmpcpp") },
+	{ MODKEY|ShiftMask, 		XK_s, 		spawn, 			SHCMD(SCROT " -m '/usr/home/fa2_l/Screenshots/screenshot-full-%d-%m-%y-%H-%M-%S.png'") },
+	{ MODKEY|ShiftMask, 		XK_f, 		spawn, 			SHCMD(SCROT " -ub '/usr/home/fa2_l/Screenshots/screenshot-window-%d-%m-%y-%H-%M-%S.png'") },
+	{ MODKEY|ShiftMask, 		XK_d, 		spawn, 			SLCMD(" scrot -q 100 -s '/usr/home/fa2_l/Screenshots/screenshot-%d-%m-%y-%H-%m-%S.png'") },
+	{ MODKEY, 			XK_z, 		spawn, 			SHCMD("ibus engine m17n:vi:telex") },
+	{ MODKEY|ShiftMask, 		XK_z, 		spawn, 			SHCMD("ibus engine xkb:us::eng") },
+	{ MODKEY|ShiftMask, 		XK_Up, 		spawn, 			SHCMD("amixer set Master 1%+") },
+	{ MODKEY|ShiftMask, 		XK_Down, 	spawn, 			SHCMD("amixer set Master 1%-") },
+	{ MODKEY|ShiftMask, 		XK_l, 		spawn, 			SHCMD(TERM " -e doas shutdown -P now") },
+	{ MODKEY|ShiftMask, 		XK_e, 		spawn, 			SHCMD("pcmanfm") },
+	{ MODKEY, 			XK_b, 		togglebar, 		{0} },
+	{ MODKEY, 			XK_space, 	zoom, 			{0} },
+	{ MODKEY, 			XK_Tab, 	focusstack, 	{.i = +1 } },
+	{ MODKEY, 			XK_i, 		incnmaster, 	{.i = +1 } },
+	{ MODKEY, 			XK_d, 		incnmaster, 	{.i = -1 } },
+	{ MODKEY, 			XK_h, 		setmfact, 		{.f = -0.05} },
+	{ MODKEY, 			XK_l, 		setmfact, 		{.f = +0.05} },
+	{ MODKEY, 			XK_x, 		killclient, 	{0} },
+	{ MODKEY, 			XK_t, 		setlayout, 		{.v = &layouts[0]} },
+	{ MODKEY, 			XK_f, 		setlayout, 		{.v = &layouts[1]} },
+	{ MODKEY, 						XK_m, 		setlayout, 		{.v = &layouts[2]} },
+	{ MODKEY|ShiftMask, 			XK_space, 	setlayout, 		{0} },
+	{ MODKEY|ShiftMask, 			XK_t, 		togglefloating,	{0} },
+	{ MODKEY, 						XK_0, 		view, 			{.ui = ~0 } },
+	{ MODKEY|ShiftMask, 			XK_0, 		tag, 			{.ui = ~0 } },
+	{ MODKEY, 						XK_comma, 	focusmon, 		{.i = -1 } },
+	{ MODKEY, 						XK_period, 	focusmon, 		{.i = +1 } },
+	{ MODKEY|ShiftMask, 			XK_comma, 	tagmon, 		{.i = -1 } },
+	{ MODKEY|ShiftMask, 			XK_period, 	tagmon, 		{.i = +1 } },
+	TAGKEYS( 						XK_1, 						0)
+	TAGKEYS( 						XK_2, 						1)
+	TAGKEYS( 						XK_3, 						2)
+	TAGKEYS( 						XK_4, 						3)
+	TAGKEYS( 						XK_5, 						4)
+	TAGKEYS( 						XK_6, 						5)
+	TAGKEYS( 						XK_7, 						6)
+	TAGKEYS( 						XK_8, 						7)
+	TAGKEYS( 						XK_9, 						8)
+	{ MODKEY|ShiftMask, 			XK_q, 		quit, 			{0} },
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
-	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	/* click 			event mask 	button 			function 		argument */
+	{ ClkLtSymbol, 		0, 			Button1, 		setlayout, 		{0} },
+	{ ClkLtSymbol, 		0, 			Button3, 		setlayout, 		{.v = &layouts[2]} },
+	{ ClkClientWin, 	MODKEY, 	Button1, 		movemouse,		{0} },
+	{ ClkClientWin, 	MODKEY, 	Button2, 		togglefloating, {0} },
+	{ ClkClientWin, 	MODKEY, 	Button3, 		resizemouse, 	{0} },
+	{ ClkTagBar, 		0, 			Button1, 		view, 			{0} },
+	{ ClkTagBar, 		0, 			Button3, 		toggleview, 	{0} },
+	{ ClkTagBar, 		MODKEY, 	Button1, 		tag, 			{0} },
+	{ ClkTagBar, 		MODKEY, 	Button3,		toggletag, 		{0} },
 };
 
